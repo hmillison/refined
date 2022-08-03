037ebb82ce063b051e343714727e3d3a6c0981a5
Henry Millison <dev@henrymillison.com>
2022-08-02 23:38:37 -0400

    simplify and use node

--- api/whatsapp.ts
+++ api/whatsapp.ts
@@ -1,7 +1,5 @@
-import { Handler } from 'https://deno.land/std@0.150.0/http/server.ts';
+export default (req, res) => {
+  const verification = req.query['hub.challenge'];
 
-export default <Handler>((req) => {
-  const u = new URL(req.url);
-  const verification = u.searchParams.get('hub.challenge');
-  return new Response(`value="${verification}"`);
-});
+  res.status(200).send(verification);
+};
